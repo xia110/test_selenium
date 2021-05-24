39ef492f6c3d71a0e0485a78a56b737283c5baea
xiahengsheng <1104642980@qq.com>
2021-05-24 10:21:28 +0800

    优化报告生成饼状图

--- src/test/java/com/xia/test_baidu.java
+++ src/test/java/com/xia/test_baidu.java
@@ -2,6 +2,8 @@ package com.xia;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,23 +15,25 @@ import java.util.concurrent.TimeUnit;
  * @create 2021-05-14 10:04
  */
 public class test_baidu {
-    @BeforeClass
+    @Test(description = "进入百度页面")
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://baidu.com");
+        Reporter.log("测试日志");
+        Assert.assertTrue(true);
         driver.close();
     }
 
-    @AfterClass
-    public void tearup(){
-        System.out.println("这是模块A结束");
+    @Test(description = "错误")
+    public void test1(){
+        Assert.fail("错误");
     }
 
-    @Test
-    public void test1(){
-        System.out.println("这是用例1");
+    @Test(description = "错误")
+    public void test2(){
+        Assert.assertTrue(true);
     }
 }
